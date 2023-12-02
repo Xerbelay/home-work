034061fd9a17080ba3c04a3299fd6e2d16b780f5
Xerbelay <148629656+Xerbelay@users.noreply.github.com>
2023-12-02 16:54:04 +0300

    что-то

---
+++ home-work/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <p>{{ text }}</p>
+  <button @click="change">измени текст</button>
+  <button @click="change2">измени текст2</button>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      text: "что-то"
+    }
+  },
+  methods: {
+    change(){
+      this.text = "ничего нет"
+    },
+    change2(){
+      this.text = "ничего"
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
